6491146f3c4e916823937888df66b072f4c03aac
Alexander <coldnexus404@gmail.com>
2024-04-15 20:42:44 +0300

    refactored net

--- src/net.cpp
+++ src/net.cpp
@@ -24,6 +24,7 @@ Net::Net(std::initializer_list<Index>              layers_sizes,
 };
 
 Net::Vector Net::Predict(const Vector& v) const {
+    assert(!layers_.empty() && "Net in NOT initialized");
     Vector temp = v;
     for (size_t i = 0; i < layers_.size(); ++i) {
         temp = layers_[i].Calculate(temp);
@@ -32,109 +33,101 @@ Net::Vector Net::Predict(const Vector& v) const {
 }
 
 void Net::Train(const std::vector<Vector>& data, const std::vector<Vector>& ans, LossFunction loss,
-                int epochs) {
+                int epochs, double tol, bool debug) {
 
-    Scalar alpha = 0.001;
-    Scalar beta1 = 0.9;
-    Scalar beta2 = 0.999;
-    Scalar eps = 1e-8;
+    assert(!layers_.empty() && "Net in NOT initialized");
+    assert(epochs >= 1 && "epochs should be a positive integer");
 
-    std::vector<Matrix> a_modifications;
-    std::vector<Vector> b_modifications;
+    Adam                optimizer = InitAdam();
+    std::vector<Vector> outputs(layers_.size() + 1);
+    long long           time = 0;
+    Scalar              dist = 0;
 
-    std::vector<Matrix> m_a;
-    std::vector<Vector> m_b;
-    std::vector<Matrix> m_at;
-    std::vector<Vector> m_bt;
-
-    std::vector<Matrix> v_a;
-    std::vector<Vector> v_b;
-    std::vector<Matrix> v_at;
-    std::vector<Vector> v_bt;
-
-    a_modifications.reserve(layers_.size());
-    m_a.reserve(layers_.size());
-    v_a.reserve(layers_.size());
-    m_at.reserve(layers_.size());
-    v_at.reserve(layers_.size());
-
-    b_modifications.reserve(layers_.size());
-    m_b.reserve(layers_.size());
-    v_b.reserve(layers_.size());
-    m_bt.reserve(layers_.size());
-    v_bt.reserve(layers_.size());
-
-    for (size_t i = 0; i < layers_.size(); ++i) {
-        a_modifications.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
-        b_modifications.emplace_back(Vector::Zero(layers_[i].OutSize()));
-
-        m_a.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
-        m_b.emplace_back(Vector::Zero(layers_[i].OutSize()));
-        v_a.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
-        v_b.emplace_back(Vector::Zero(layers_[i].OutSize()));
-        m_at.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
-        m_bt.emplace_back(Vector::Zero(layers_[i].OutSize()));
-        v_at.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
-        v_bt.emplace_back(Vector::Zero(layers_[i].OutSize()));
-    }
-
-    std::vector<Vector> x(layers_.size());
-    Vector              tmp;
-    Vector              res;
-    VectorT             u;
-
-    Scalar dist = 0;
-
-    for (int t = 1; t <= epochs; ++t) {
+    for (int cur_epoch = 1; cur_epoch <= epochs; ++cur_epoch) {
         for (size_t i = 0; i < data.size(); ++i) {
-            tmp = data[i];
-            for (size_t j = 0; j < layers_.size(); ++j) {
-                x[j] = tmp;
-                tmp = layers_[j].Calculate(tmp);
-            }
-            dist += loss.Dist(tmp, ans[i]);
-            
-            if (i % (data.size()/10) == 0) {
-                std::cout << i / (data.size()/10) << " happened of epoch\n";
-                std::cout << loss.Dist(tmp, ans[i]) << '\n';
-            }
+            ++time;
+            Forward(data[i], &outputs);
+            dist += loss.Dist(data.back(), ans[i]);
+            Backward(outputs, ans[i], loss, &optimizer);
+            Update(optimizer, time);
 
-            u = loss.Gradient(tmp, ans[i]);
-
-            for (size_t j = 0; j < layers_.size(); ++j) {
-                size_t k = layers_.size() - j - 1;
-                a_modifications[k] += layers_[k].GetDa(u, x[k]);
-                b_modifications[k] += layers_[k].GetDb(u, x[k]);
-                u = layers_[k].Propagate(u, x[k]);
-            }
-
-            for (size_t k = 0; k < layers_.size(); ++k) {
-                m_a[k] = beta1 * m_a[k] + (1 - beta1) * a_modifications[k];
-                v_a[k] = beta2 * v_a[k] +
-                         (1 - beta2) * a_modifications[k].cwiseProduct(a_modifications[k]);
-
-                m_b[k] = beta1 * m_b[k] + (1 - beta1) * b_modifications[k];
-                v_b[k] = beta2 * v_b[k] +
-                         (1 - beta2) * b_modifications[k].cwiseProduct(b_modifications[k]);
-
-                m_at[k] = m_a[k] / (1 - std::pow(beta1, t));
-                v_at[k] = (v_a[k] / (1 - std::pow(beta2, t))).cwiseSqrt();
-                v_at[k].array() += eps;
-
-                m_bt[k] = m_b[k] / (1 - std::pow(beta1, t));
-                v_bt[k] = (v_b[k] / (1 - std::pow(beta2, t))).cwiseSqrt();
-                v_bt[k].array() += eps;
-            }
-            for (size_t i = 0; i < layers_.size(); ++i) {
-                layers_[i].UpdateA(m_at[i].cwiseProduct(v_at[i].cwiseInverse()), -alpha);
-                layers_[i].UpdateB(m_bt[i].cwiseProduct(v_bt[i].cwiseInverse()), -alpha);
-                a_modifications[i].setZero();
-                b_modifications[i].setZero();
+            if (debug && i % (data.size() / 10) == 0) {
+                std::cout << 10 * i / (data.size() / 10) << " happened of epoch\n";
             }
         }
-        std::cout << "epoch " << t << '\n';
-        std::cout << "dist " << dist / data.size() << '\n';
-        dist = 0;
+        if (debug) {
+            std::cout << cur_epoch << " epoch ended\n";
+            std::cout << "average dist " << dist / data.size() << '\n';
+        }
+        if (dist / data.size() < tol) {
+            if (debug) {
+                std::cout << "converged\n";
+            }
+            return;
+        }
+    }
+}
+
+Net::Adam Net::InitAdam() {
+
+    Adam ret;
+    ret.m_a.reserve(layers_.size());
+    ret.v_a.reserve(layers_.size());
+    ret.m_b.reserve(layers_.size());
+    ret.v_b.reserve(layers_.size());
+
+    for (size_t i = 0; i < layers_.size(); ++i) {
+        ret.m_a.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
+        ret.m_b.emplace_back(Vector::Zero(layers_[i].OutSize()));
+        ret.v_a.emplace_back(Matrix::Zero(layers_[i].OutSize(), layers_[i].InSize()));
+        ret.v_b.emplace_back(Vector::Zero(layers_[i].OutSize()));
+    }
+    return ret;
+}
+
+void Net::Forward(const Vector& inp, std::vector<Vector>* x) {
+    (*x)[0] = inp;
+    for (size_t j = 0; j < layers_.size(); ++j) {
+        (*x)[j + 1] = layers_[j].Calculate((*x)[j]);
+    }
+}
+
+void Net::Backward(const std::vector<Vector> outputs, const Vector& ans, const LossFunction& loss,
+                   Adam* opt) {
+
+    VectorT u;
+    u = loss.Gradient(outputs.back(), ans);
+
+    Matrix da;
+    Vector db;
+    for (size_t j = 0; j < layers_.size(); ++j) {
+        size_t k = layers_.size() - j - 1;
+
+        da = layers_[k].GetDa(u, outputs[k]);
+        db = layers_[k].GetDb(u, outputs[k]);
+
+        (*opt).m_a[k] = (*opt).beta1 * (*opt).m_a[k] + (1 - (*opt).beta1) * da;
+        (*opt).v_a[k] = (*opt).beta2 * (*opt).v_a[k] + (1 - (*opt).beta2) * da.cwiseProduct(da);
+
+        u = layers_[k].Propagate(u, outputs[k]);
+    }
+}
+
+void Net::Update(const Adam& opt, long long t) {
+    Matrix m_hat_a;
+    Matrix v_hat_a;
+    Vector m_hat_b;
+    Vector v_hat_b;
+    for (size_t k = 0; k < layers_.size(); ++k) {
+        m_hat_a = opt.m_a[k] / (1 - std::pow(opt.beta1, t));
+        v_hat_a = (opt.v_a[k] / (1 - std::pow(opt.beta2, t))).cwiseSqrt();
+        v_hat_a.array() += opt.eps;
+        layers_[k].UpdateA(m_hat_a.cwiseProduct(v_hat_a.cwiseInverse()), -opt.alpha);
+
+        m_hat_b = opt.m_b[k] / (1 - std::pow(opt.beta1, t));
+        v_hat_b = (opt.v_b[k] / (1 - std::pow(opt.beta2, t))).cwiseSqrt();
+        v_hat_b.array() += opt.eps;
+        layers_[k].UpdateB(m_hat_b.cwiseProduct(v_hat_b.cwiseInverse()), -opt.alpha);
     }
 }
 
--- src/net.h
+++ src/net.h
@@ -20,12 +20,31 @@ public:
     Net(std::initializer_list<Index>, std::initializer_list<ActivationFunction>);
 
     void Train(const std::vector<Vector>& data, const std::vector<Vector>& ans, LossFunction loss,
-               int epochs);
+               int epochs, double tol = 1e-8, bool debug = false);
 
     Vector Predict(const Vector& v) const;
 
     friend std::ostream& operator<<(std::ostream& stream, const Net& net);
     friend std::istream& operator>>(std::istream& stream, Net& net);
+
+private:
+    struct Adam {
+        Scalar alpha = 0.001;
+        Scalar beta1 = 0.9;
+        Scalar beta2 = 0.999;
+        Scalar eps = 1e-8;
+
+        std::vector<Matrix> m_a;
+        std::vector<Vector> m_b;
+        std::vector<Matrix> v_a;
+        std::vector<Vector> v_b;
+    };
+
+    Adam InitAdam();
+    void Forward(const Vector& inp, std::vector<Vector>* outputs);
+    void Backward(const std::vector<Vector> outputs, const Vector& ans, const LossFunction& loss,
+                  Adam* optimizer);
+    void Update(const Adam& optimizer, long long time);
 
 private:
     std::vector<Layer> layers_;
